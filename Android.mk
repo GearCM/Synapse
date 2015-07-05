34c46198b1cd98ea0153ceb5f78f327c9df8d53c
Alberto96 <albertoguanti@gmail.com>
2015-07-06 00:05:54 +0200

    Synapse: completely reorganize repo
    * fixed in-tree package compiling
    * updated gradle build tools

--- Android.mk
+++ Android.mk
@@ -1,31 +1,18 @@
-LOCAL_PATH:= $(call my-dir)
+# Copyright (C) 2014 The NamelessRom Project
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/src/main/res
-LOCAL_MANIFEST_FILE := src/main/AndroidManifest.xml
-
-LOCAL_MODULE_TAGS := tests
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
-LOCAL_STATIC_JAVA_LIBRARIES := \
-		android-support-v4 \
-		json-smart \
-		systembartint \
-
-LOCAL_PACKAGE_NAME := Synapse
-
-#LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard.flags
-
-#LOCAL_PRIVILEGED_MODULE := true
-
-include $(BUILD_PACKAGE)
-
-##################################################
-include $(CLEAR_VARS)
-
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-		json-smart:libs/json-smart-1.2.jar \
-		systembartint:libs/systembartint-1.0.3.jar \
-
-include $(BUILD_MULTI_PREBUILT)
+include $(LOCAL_PATH)/app/src/main/Android.mk
